ec95141b26338d30ffaa3db3cf9cb9932502fbd8
SJkim <tjrwn1247@gmail.com>
2022-03-14 23:56:02 +0900

    js 상수

---
+++ JS/상수/script.js
@@ -0,0 +1,12 @@
+let aaa;
+aaa = 5;
+console.log(aaa);
+aaa = 8;
+console.log(aaa);
+
+const bb = 10;
+console.log(bb);
+
+const promptMessage = "프롬프트 메시지!"
+const result = prompt(promptMessage);
+console.log(result);
