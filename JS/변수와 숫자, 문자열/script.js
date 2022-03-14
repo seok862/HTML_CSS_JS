0bd8bee6d828449cb147e441dc440533b07fb76d
SJkim <tjrwn1247@gmail.com>
2022-03-14 20:11:32 +0900

    변수

---
+++ JS/변수와 숫자, 문자열/script.js
@@ -0,0 +1,11 @@
+console.log(21);
+console.log(3.14);
+console.log(-3.14);
+console.log('아무말');
+console.log("아무말");
+
+let number1 = 14;
+console.log(number1);
+
+let string1 = '아아어';
+console.log(string1);
